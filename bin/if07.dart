327175606a5dcd6aa1e28957d91dbc4b9e312585
ogabek <norpulatovogabek42@gmail.com>
2022-06-28 18:24:23 +0500

    task7

--- bin/if07.dart
+++ bin/if07.dart
@@ -11,4 +11,37 @@
         a: integer
     Returns:
         string: the message to print */
-void main() {}
+String func(number) {
+  String q = 'positive odd number ';
+  String w = 'positive even number ';
+  String e = 'negative odd number ';
+  String r = 'negative even number';
+  String t = 'the number is zero';
+  if (number > 0) {
+    if (number % 2 == 1) {
+      return q;
+    }
+  }
+  if (number > 0) {
+    if (1 - number % 2 == 1) {
+      return w;
+    }
+  }
+  if (number < 0) {
+    if (number % 2 == 1) {
+      return e;
+    }
+  }
+  if (number < 0) {
+    if (1 - number % 2 == 1) {
+      return r;
+    }
+  }
+  if (number == 0) {
+    return t;
+  }
+}
+
+void main() {
+  print(func(6438));
+}
