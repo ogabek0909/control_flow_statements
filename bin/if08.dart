f08dbdb95d568ca3e40aac26499afda75bbfa9bd
ogabek <norpulatovogabek42@gmail.com>
2022-06-28 18:49:56 +0500

    task8

--- bin/if08.dart
+++ bin/if08.dart
@@ -10,5 +10,33 @@
         a: integer
     Returns:
         string: the message to print */
+String func(number) {
+  String q = 'two-digit odd number';
+  String w = 'two-digit even number';
+  String e = 'three-digit odd number';
+  String r = 'three-digit even number';
+  if (100 > number && number > 9) {
+    if (number % 2 == 1) {
+      return q;
+    }
+  }
+  if (100 > number && number > 9) {
+    if (1 - number % 2 == 1) {
+      return w;
+    }
+  }
+  if (1000 > number && number > 100) {
+    if (number % 2 == 1) {
+      return e;
+    }
+  }
+  if (1000 > number && number > 100) {
+    if (1 - number % 2 == 1) {
+      return r;
+    }
+  }
+}
 
-void main() {}
+void main() {
+  print(func(388));
+}
