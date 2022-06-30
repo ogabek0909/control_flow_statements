aa238b1d4af8f3fcbd336faff3bc72a66a5632bb
ogabek <norpulatovogabek42@gmail.com>
2022-06-30 17:46:11 +0500

    task

---
+++ bin/7.dart
@@ -0,0 +1,9 @@
+void main() {
+  String x = 'Assalomu alaykum';
+
+  if (x == "Assalomu alaykum") {
+    print("Va alaykum assalom");
+  } else if (x == "Qalaysan") {
+    print("Yaxshi, rahmat!");
+  }
+}
