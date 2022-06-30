5c621407a5584db6a3c6fe214be68cf17fd8de05
ogabek <norpulatovogabek42@gmail.com>
2022-06-30 17:29:58 +0500

    task

---
+++ bin/6.dart
@@ -0,0 +1,11 @@
+void main() {
+  int x = 12, v = 12, n = 13;
+  if (x > v) {
+    if (x < n) {
+      print('Og\'abek');
+    }
+  } else if (v < n) {
+    print('Norpulatov');
+  } else
+    print('N.O');
+}
